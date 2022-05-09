365862366438f4e96a57bdb99945221bdf312379
caiqichang <cai_qichang@qq.com>
2022-05-09 17:08:42 +0800

    .

--- spring-app/src/main/kotlin/app/spring/common/util/RestTemplateUtil.kt
+++ spring-app/src/main/kotlin/app/spring/common/util/RestTemplateUtil.kt
@@ -5,7 +5,10 @@ import org.springframework.http.client.SimpleClientHttpRequestFactory
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
 import java.net.HttpURLConnection
+import java.security.cert.X509Certificate
 import javax.net.ssl.HttpsURLConnection
+import javax.net.ssl.SSLContext
+import javax.net.ssl.X509TrustManager
 
 enum class RestTemplateUtil {
     INSTANCE;
@@ -21,7 +24,27 @@ enum class RestTemplateUtil {
         val factory = if (ignoreSSL) {
             object : SimpleClientHttpRequestFactory() {
                 override fun prepareConnection(connection: HttpURLConnection, httpMethod: String) {
-                    if (connection is HttpsURLConnection) connection.setHostnameVerifier { _, _ -> true }
+                    if (connection is HttpsURLConnection) {
+                        // disable hostname verification
+                        connection.setHostnameVerifier { _, _ -> true }
+
+                        // disable certification checking
+                        val sslContext = SSLContext.getInstance("SSL")
+                        sslContext.init(null, Array(1) {
+                            object : X509TrustManager {
+                                override fun checkClientTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                                }
+
+                                override fun checkServerTrusted(chain: Array<out X509Certificate>?, authType: String?) {
+                                }
+
+                                override fun getAcceptedIssuers(): Array<X509Certificate>? {
+                                    return null
+                                }
+                            }
+                        }, null)
+                        connection.sslSocketFactory = sslContext.socketFactory
+                    }
                     super.prepareConnection(connection, httpMethod)
                 }
             }
