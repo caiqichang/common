3142951d88d48668b7797f439f7606f9e37a33f4
caiqichang <cai_qichang@qq.com>
2022-06-05 15:37:17 +0800

    .

--- spring-cloud/service2/src/main/kotlin/spring/service2/TestController.kt
+++ spring-cloud/service2/src/main/kotlin/spring/service2/TestController.kt
@@ -1,14 +1,21 @@
 package spring.service2
 
+import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/test")
 class TestController {
 
+    companion object {
+        private val log = LoggerFactory.getLogger(TestController::class.java)
+    }
+
     @RequestMapping("/t0")
-    fun t0(): String {
+    fun t0(request: HttpServletRequest): String {
+        log.info(request.getHeader("fromClient"))
         return "call service 2 t0"
     }
 }
